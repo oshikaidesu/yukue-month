5e3fbf1dc5b5d6c75c5473dc68656b9852671fc2
hottaOT <oshikai.desu@gmail.com>
2025-09-02 10:04:05 +0900

    自動デプロイ: 2025-09-02 10:04:05

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -55,7 +55,7 @@ export default function Header() {
               </li>
               {/* ニコニコマイリストリンク */}
               <li>
-                <Link href="https://www.nicovideo.jp/user/12345678/mylist/12345678" className="flex items-center gap-3">
+                <Link href="https://www.nicovideo.jp/mylist/76687470" className="flex items-center gap-3">
                   <svg className="w-4.5 h-4.5" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <path d="M.4787 7.534v12.1279A2.0213 2.0213 0 0 0 2.5 21.6832h2.3888l1.323 2.0948a.4778.4778 0 0 0 .4043.2205.4778.4778 0 0 0 .441-.2205l1.323-2.0948h6.9828l1.323 2.0948a.4778.4778 0 0 0 .441.2205c.1838 0 .3308-.0735.4043-.2205l1.323-2.0948h2.6462a2.0213 2.0213 0 0 0 2.0213-2.0213V7.5339a2.0213 2.0213 0 0 0-2.0213-1.9845h-7.681l4.4468-4.4469L17.1637 0l-5.1452 5.1452L6.8 0 5.6973 1.1025l4.4102 4.4102H2.5367a2.0213 2.0213 0 0 0-2.058 2.058z"/>
                   </svg>
@@ -149,7 +149,7 @@ export default function Header() {
 
           {/* ニコニコマイリストリンク */}
           <li>
-            <Link href="https://www.nicovideo.jp/user/12345678/mylist/12345678" className="flex items-center gap-3">
+            <Link href="https://www.nicovideo.jp/mylist/76687470" className="flex items-center gap-3">
               <svg className="w-4.5 h-4.5" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <g transform="translate(2,0)">
                   <path d="M.4787 7.534v12.1279A2.0213 2.0213 0 0 0 2.5 21.6832h2.3888l1.323 2.0948a.4778.4778 0 0 0 .4043.2205.4778.4778 0 0 0 .441-.2205l1.323-2.0948h6.9828l1.323 2.0948a.4778.4778 0 0 0 .441.2205c.1838 0 .3308-.0735.4043-.2205l1.323-2.0948h2.6462a2.0213 2.0213 0 0 0 2.0213-2.0213V7.5339a2.0213 2.0213 0 0 0-2.0213-1.9845h-7.681l4.4468-4.4469L17.1637 0l-5.1452 5.1452L6.8 0 5.6973 1.1025l4.4102 4.4102H2.5367a2.0213 2.0213 0 0 0-2.058 2.058z"/>
